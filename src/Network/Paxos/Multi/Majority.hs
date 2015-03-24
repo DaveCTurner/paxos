cba49490bf3551d48bf374a835c071bcb60801ac
David Turner <dave.c.turner@gmail.com>
2015-03-24 15:14:02 +0000

    Whitespace

--- src/Network/Paxos/Multi/Majority.hs
+++ src/Network/Paxos/Multi/Majority.hs
@@ -20,8 +20,10 @@ instance Quorum MajorityOf where
   type Alteration MajorityOf = MajorityAlteration
 
   noQuorums = MajorityOf S.empty
+
   isQuorum (MajorityOf voters) s
       = not (S.null voters)
       && S.size (S.intersection voters s) * 2 > S.size voters
+
   alterQuorum (AddAcceptor    a) (MajorityOf vs) = MajorityOf (S.insert a vs)
   alterQuorum (RemoveAcceptor a) (MajorityOf vs) = MajorityOf (S.delete a vs)
