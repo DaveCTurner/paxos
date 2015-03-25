439532e5b5f2748b9817794c6b92271658172ad1
David Turner <dave.c.turner@gmail.com>
2015-03-25 06:15:45 +0000

    EmitterT now has Writer and State

--- src/Network/Paxos/Multi/Emitter.hs
+++ src/Network/Paxos/Multi/Emitter.hs
@@ -3,24 +3,35 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeFamilies #-}
 
-module Network.Paxos.Multi.Emitter where
+module Network.Paxos.Multi.Emitter
+  ( EmitterT
+  , runEmitterT
+  , execEmitterT
+  ) where
 
 import qualified Data.DList as DL
-import Control.Arrow
 import Control.Applicative
 import Control.Monad.Writer
+import Control.Monad.State
 
 import Network.Paxos.Multi.Types
 
-newtype EmitterT w m a = EmitterT (WriterT (DL.DList w) m a)
+newtype EmitterT w s m a = EmitterT (WriterT (DL.DList w) (StateT s m) a)
   deriving (Functor, Applicative, Monad)
 
-instance Monad m => MonadEmitter (EmitterT w m) where
-  type Emitted (EmitterT w m) = w
+instance MonadTrans (EmitterT w s) where
+  lift = EmitterT . lift . lift
+
+instance Monad m => MonadState s (EmitterT w s m) where
+
+instance Monad m => MonadEmitter (EmitterT w s m) where
+  type Emitted (EmitterT w s m) = w
   emit = EmitterT . tell . DL.singleton
 
-runEmitterT :: Monad m => EmitterT w m a -> m (a, [w])
-runEmitterT (EmitterT go) = liftM (second DL.toList) $ runWriterT go
+runEmitterT :: Monad m => EmitterT w s m a -> s -> m (a, [w], s)
+runEmitterT (EmitterT go) s = do
+  ((a,dw),s') <- runStateT (runWriterT go) s
+  return (a, DL.toList dw, s')
 
-execEmitterT :: Monad m => EmitterT w m a -> m [w]
-execEmitterT = liftM snd . runEmitterT
+execEmitterT :: Monad m => EmitterT w s m a -> s -> m ([w], s)
+execEmitterT go s = liftM (\(_,w,s') -> (w,s')) $ runEmitterT go s
